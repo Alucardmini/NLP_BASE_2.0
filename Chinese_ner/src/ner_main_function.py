d706ce4ff694ab6c5219c4c42c0811e6dda9320d
wuxikun <linuxjade@163.com>
2018-12-27 19:29:17 +0800

    ner

--- Chinese_ner/src/ner_main_function.py
+++ Chinese_ner/src/ner_main_function.py
@@ -12,6 +12,13 @@ from keras_contrib.layers.crf import CRF
 # from keras_contrib.utils import save_load_utils
 from Chinese_ner.src.ner_datas import ner_datas
 from keras.preprocessing import sequence
+from keras import optimizers
+from keras.utils import np_utils
+
+
+from keras import backend as K
+K.set_image_dim_ordering('tf')
+
 
 
 if __name__ == "__main__":
@@ -32,12 +39,17 @@ if __name__ == "__main__":
     train_vocabs = sequence.pad_sequences(train_vocabs, max_len)
     test_vocabs = sequence.pad_sequences(test_vocabs, max_len)
 
-    train_labels = sequence.pad_sequences(train_labels, max_len)
-    test_labels = sequence.pad_sequences(test_labels, max_len)
-    train_labels = train_labels.reshape(train_labels.shape[0], train_labels.shape[-1], 1)
-    test_labels = test_labels.reshape(test_labels.shape[0], test_labels.shape[-1], 1)
-    print( train_vocabs.shape)
-    print( train_labels.shape)
+
+    train_labels = sequence.pad_sequences(train_labels, max_len, dtype='float')
+    test_labels = sequence.pad_sequences(test_labels, max_len, dtype='float')
+
+    train_labels = np_utils.to_categorical(train_labels, num_class)
+    test_labels = np_utils.to_categorical(test_labels, num_class)
+
+    # train_labels = train_labels.reshape(train_labels.shape[0], train_labels.shape[-1], 1)
+    # test_labels = test_labels.reshape(test_labels.shape[0], test_labels.shape[-1], 1)
+    print(train_vocabs.shape)
+    print(train_labels.shape)
 
     model = Sequential()
     # model.add(Input(shape=(None,), dtype='int32'))
@@ -48,32 +60,27 @@ if __name__ == "__main__":
     model.add(Dropout(dropout_rate))
     # model.add(TimeDistributed(Dense(num_class)))
     model.add(TimeDistributed(Dense(num_class)))
+    crf_layer = CRF(num_class)
+    model.add(crf_layer)
+
+    optmr = optimizers.Adam(lr=0.001, beta_1=0.5)
+
     model.compile(
         optimizer='rmsprop',
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
+        loss=crf_layer.loss_function,
+        metrics=[crf_layer.accuracy]
     )
-
-    # crf_layer = CRF(num_class)
-    # # model.add(crf_layer)
-    # model.add(TimeDistributed(crf_layer))
-    # model.compile(
-    #     optimizer='rmsprop',
-    #     loss=crf_layer.loss_function,
-    #     metrics=[crf_layer.accuracy]
-    # )
 
 
     print(model.summary())
-    # model.fit(train_vocabs, train_labels, batch_size=128, epochs=1)
-    # model.save(model_path)
+    model.fit(train_vocabs, train_labels, batch_size=128, epochs=1)
 
     test_content = "北京到上海多少公里"
     pred_x = data_loader.vector_sent_only(list(test_content))
 
     y = model.predict(pred_x)
     print(y.shape)
-    print(y[1])
-    # print(y)
+    print(train_labels[0])
+    print(y)
 
 
